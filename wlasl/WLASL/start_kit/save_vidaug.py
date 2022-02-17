2e96d4960b3583c6ac3dedd4ca90eed19343ae26
yyhyunhyun <ryh2803@gmail.com>
2022-02-17 18:22:34 +0900

    preprocessing function more flexible directory

--- wlasl/WLASL/start_kit/save_vidaug.py
+++ wlasl/WLASL/start_kit/save_vidaug.py
@@ -7,11 +7,11 @@ from vid.gen_key_arr import KeyArrGen
 from vid.vidaug import VidAug
 
 
-def main():
+def main(data="train"):
     aug_time = []
     s_time = time.time()
     try: 
-        for i in load_data_json():
+        for i in load_data_json(data):
             j = 0
             while j < len(i['files']):
                 count = 0
@@ -21,7 +21,7 @@ def main():
                     # print(i['gloss'], len(i['files']))
                     # print(i['files'][j])
 
-                    key_arr = save_augvid2keyarr(i['files'][j], count)
+                    key_arr = save_augvid2keyarr(i['files'][j], count, data)
                     if key_arr is None: continue
                     print(key_arr.shape)
                     aug_time.append({len(key_arr): time.time() - st_time})
@@ -30,23 +30,24 @@ def main():
     except KeyboardInterrupt as e:
         print(aug_time)
     finally:
-        with open("./processing_time.json", 'w') as f:
+        with open(f"./processing_time_{data}.json", 'w') as f:
             json.dump(aug_time,f)
         print(time.time() - s_time)
 
-def save_test():
+def save_without_aug(dir = "test"):
     process_time = []
     s_time = time.time()
-    for i in load_data_json("test"):
+    for i in load_data_json(dir):
         print(i)
         for j in i['files']:
+            print(j)
             st_time = time.time()
             key_arr = save_purevid2keyarr(j)
             if key_arr is None: continue
             print(key_arr.shape)
             process_time.append({len(key_arr): time.time() - st_time})
     
-    with open("./valid_processing_time.json", 'w') as f:
+    with open(f"./{dir}_processing_time.json", 'w') as f:
             json.dump(process_time,f)
     
     print(time.time() - s_time)
@@ -62,14 +63,14 @@ def save_purevid2keyarr(file_path):
     save_path = os.path.join(save_dir,vid_name)+ ".pickle"
     pickle.dump(key_arr,open(save_path,'wb'))
 
-
     return key_arr
 
-def aug_train_mkdir(gloss):
-    path = f"./train/{gloss}"
+def aug_train_mkdir(gloss, dir="train"):
+    path = f"./{dir}/{gloss}"
     if not os.path.isdir(path):
         os.mkdir(path)
     return path
+
 def aug_valid_n_test_mkdir(gloss):
     path = f"./test/{gloss}"
     if not os.path.isdir(path):
@@ -77,7 +78,7 @@ def aug_valid_n_test_mkdir(gloss):
     return path
 
 
-def save_augvid2keyarr(file_path, count):
+def save_augvid2keyarr(file_path, count, dir="train"):
 
     # vid aug
     vid = VidAug(file_path)
@@ -91,16 +92,17 @@ def save_augvid2keyarr(file_path, count):
     if len(key_arr) < 15: return None
 
 
-    save_dir = aug_train_mkdir(file_path.split(os.sep)[-2])
+    save_dir = aug_train_mkdir(file_path.split(os.sep)[-2], dir)
     save_path = os.path.join(save_dir,vid_name)+ f"-{count}.pickle"
     pickle.dump(key_arr,open(save_path,'wb'))
-
 
     return key_arr
 
 
 
 if __name__ =="__main__":
-    # main()
-    save_test()
-    # aug_train_mkdir("all")
+    dir = ["own_vid",'more_vid','train']
+    for i in dir:
+        print(i)
+        save_without_aug(i)
+    
